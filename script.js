81539bb7d5bf01d1e720d31cb63a2a92fd6dcd6c
GiulioDangelo <dangelogiulio48@gmail.com>
2023-05-02 22:33:33 +0200

    add comment

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 const images = [
 {
-    image: "img/01.webp",
-    title: "Marvel's Spiderman Miles Morale",
-    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+	image: "img/01.webp",
+	title: "Marvel's Spiderman Miles Morale",
+	text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
 },
 {
-    image: "img/02.webp",
-    title: "Ratchet & Clank: Rift Apart",
-    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+	image: "img/02.webp",
+	title: "Ratchet & Clank: Rift Apart",
+	text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
 },
 {
-    image: "img/03.webp",
-    title: "Fortnite",
-    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+	image: "img/03.webp",
+	title: "Fortnite",
+	text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
 },
 {
-    image: "img/04.webp",
-    title: "Stray",
-    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+	image: "img/04.webp",
+	title: "Stray",
+	text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
 },
 {
-    image: "img/05.webp",
-    title: "Marvel's Avengers",
-    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+	image: "img/05.webp",
+	title: "Marvel's Avengers",
+	text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
 },
 ];
 
+
+
+// Get the container elements from the HTML
 const containerHighlighted = document.querySelector(".highlighted");
 const containerThumb = document.querySelector(".thumb");
 
+
+
+// Loop through the images array and add each image to the HTML
 for (let i = 0; i < images.length; i++) {
-	containerHighlighted.innerHTML += `<div class="img_container ${
-		i == 0 ? "active" : ""
-	}">
+	
+	containerHighlighted.innerHTML += `<div class="img_container ${i == 0 ? "active" : ""}">
     <img src="${images[i].image}" alt="" class="${i == 0 ? "active" : ""}">
     <h2 class="title">${images[i].title}</h2>
     <p class="text">${images[i].text}</p>
@@ -43,36 +48,45 @@ for (let i = 0; i < images.length; i++) {
 	}"></img>`;
 }
 
-const listHighlighted = document.querySelectorAll(
-	".highlighted .img_container"
-);
+
+
+// Get the list of image containers and thumbnail images from the HTML
+const listHighlighted = document.querySelectorAll(".highlighted .img_container");
 const listThumb = document.querySelectorAll(".thumb img");
 const backward = document.querySelector(".reverse");
-
-const btnPrev = document.querySelector(".btn-prev");
-const btnNext = document.querySelector(".btn-next");
 let activeIndex = 0;
 
+
+
+// Get the next button and add a click event listener
+const btnNext = document.querySelector(".btn-next");
 btnNext.addEventListener("click", function () {
-	listHighlighted[activeIndex].classList.remove("active");
+    listHighlighted[activeIndex].classList.remove("active");
 	listThumb[activeIndex].classList.remove("active");
-
+	
 	activeIndex++;
+	
+	// If the activeIndex is less than 0, set it to the first image in the list
+	if (activeIndex < 0) {
+		if (activeIndex >= listHighlighted.length) {
+			activeIndex = 0;
+		}
+		
+		listHighlighted[activeIndex].classList.add("active");
+		listThumb[activeIndex].classList.add("active");
+	}});
+	
+	
 
-	if (activeIndex >= listHighlighted.length) {
-		activeIndex = 0;
-	}
-
-	listHighlighted[activeIndex].classList.add("active");
-	listThumb[activeIndex].classList.add("active");
-});
-
+// Get the previus button and add a click event listener
+const btnPrev = document.querySelector(".btn-prev");
 btnPrev.addEventListener("click", function () {
 	listHighlighted[activeIndex].classList.remove("active");
 	listThumb[activeIndex].classList.remove("active");
 
 	activeIndex--;
 
+	// If the activeIndex is less than 0, set it to the last image in the list
 	if (activeIndex < 0) {
 		activeIndex = listHighlighted.length - 1;
 	}
@@ -81,20 +95,27 @@ btnPrev.addEventListener("click", function () {
 	listThumb[activeIndex].classList.add("active");
 });
 
+
+
+// Set interval to automatically switch the highlighted image every 3 seconds
 let forward = setInterval(() => {
 	listHighlighted[activeIndex].classList.remove("active");
 	listThumb[activeIndex].classList.remove("active");
 
 	activeIndex++;
 
+	// If the activeIndex is less than 0, set it to the first image in the list
 	if (activeIndex >= listHighlighted.length) {
 		activeIndex = 0;
 	}
 
 	listHighlighted[activeIndex].classList.add("active");
 	listThumb[activeIndex].classList.add("active");
-}, 3000);
+}, 2000);
 
+
+
+// Add event listener to the backward button to stop the forward interval and start a new one in reverse
 backward.addEventListener("click", function () {
 	clearInterval(forward);
 	setInterval(() => {
@@ -103,15 +124,19 @@ backward.addEventListener("click", function () {
 
 		activeIndex--;
 
+		// If the activeIndex is less than 0, set it to the last image in the list
 		if (activeIndex < 0) {
 			activeIndex = listHighlighted.length - 1;
 		}
 
 		listHighlighted[activeIndex].classList.add("active");
 		listThumb[activeIndex].classList.add("active");
-	}, 1000);
+	}, 2000);
 });
 
+
+
+// Add event listeners to the thumbnail images to switch the highlighted image when clicked
 for (let i = 0; i < listThumb.length; i++) {
 	listThumb[i].addEventListener("click", function () {
 		listHighlighted[activeIndex].classList.remove("active");
